c8d973fdb68562a6390c3f28aa1ce38ca37672f4
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-05-14 14:04:16 +0100

    data actualizado al dia

--- src/data/data.js
+++ src/data/data.js
@@ -5,14 +5,21 @@ const cookieLink = mainLink + "/sanctum/csrf-cookie";
 const loginLink = mainLink + "/api/login"
 const passwordLink = mainLink + "/api/updatepassword";
 const logoutLink = mainLink + "/api/logout"
+const getAllUsers = mainLink + "/api/users" ;
 const getVolunteers = mainLink + "/api/voluntarios";
-const pieChartVolunteerData = mainLink + "/api/v1/datos-voluntarios";
-const animalsLink = mainLink + "/api/animal";
+const updateVolunteer = mainLink  + "/api/voluntarios/";
+const getPadrinos = mainLink + "/api/padrino";
+const updatePadrinos = mainLink + "/api/padrino/"
+const getAnimales = mainLink + "/api/animal";
+const updateAnimales = mainLink + "/api/animal/"
+const getTareas = mainLink + "/api/tareas";
+const updateTareas = mainLink + "/api/tareas/"
+const getFormacion = mainLink + "/api/formacion"
+const pieChartVolunteerData = mainLink + "/api/datos-voluntarios" ;
+const animalsLink = mainLink + "/api/animal/"
 const getCsrfToken = async () => {
     const csrfResponse = await axios.get(cookieLink);
     return csrfResponse.data; // Access data property for CSRF response
 };
 
-export {
-    mainLink, cookieLink, loginLink, passwordLink, logoutLink, pieChartVolunteerData, getVolunteers, animalsLink, getCsrfToken
-}
+export {mainLink, cookieLink, loginLink, passwordLink, logoutLink, getAllUsers, pieChartVolunteerData, updateVolunteer, getVolunteers, getPadrinos, updatePadrinos, getAnimales, updateAnimales, getTareas, updateTareas, getFormacion,animalsLink, getCsrfToken}
